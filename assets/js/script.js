42ae18e82b6a9db35a356150d4045c91fdb7ab22
matheusamadeu <00001099049908sp@al.educacao.sp.gov.br>
2025-08-21 14:28:24 -0300

    criando evento de clique no botão login

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const btnLogin = document.querySelector("#btn-login");
+const btnLogin = document.querySelector("#btn-register");
+
+btnLogin.addEventListener("click", (e) => {
+    console.log("Cliquei no botão login");
+})
